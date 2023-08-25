d54272675180c38ec57d1902f1c0c1f5a5d13331
Alejandro Montero <amontero0525@gmail.com>
2023-08-25 09:47:19 -0600

    dobleenlazada.h / news.h

---
+++ dobleenlazada.h
@@ -0,0 +1,17 @@
+//? Alejandro Montero Acevedo
+//! LISTAS
+    //* Lista simplemente enlazada o doblemente enlazada: 
+        // Para generar el filtro con las palabras que el usuario proporcione
+    //* Matrices: 
+        // Para organizar el ranking de top resultados que el usuario quiera ver por su relevancia y mover los titulares
+    //* Recursion:
+        // Para la busqueda de noticias.
+//! METODOS
+    //*Insertar:
+        // noticias que contengan las palabras que el usuario introdujo
+    //*Eliminar:
+        // noticia que contengan las palabra que el usuario introdujo
+    //*Ordenar:
+        // subir o bajar un titular de relevancia en el ranking
+    //* Buscar: 
+        // los titulos con las palabras filtro proporcionadas
---
+++ news.h
@@ -0,0 +1,17 @@
+//? Alejandro Montero Acevedo
+
+//! METODOS
+    //*getTop5();
+        // Display(Int position) y que repita 5 veces
+    //*getAll();
+        // Display(Int position) y que repita hasta terminar la lista de noticias.
+    //*Buscar(string filtro):
+        // getNext()
+    //*Ocultar(string filtro)
+        // getNext()
+        // Eliminar()
+    //*Reubicar(noticia, posición):
+        // obtenerPosicion(pDato):
+        // Lenght(pDato):
+        // Agregar()
+        // Eliminar()
